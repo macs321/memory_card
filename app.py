b8780735418929e4993b834159db6d86f678b228
macs321 <macs321@gmail.com>
2024-10-06 13:31:10 +0300

    add generation new question

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from random import choice, shuffle
 from PyQt6.QtWidgets import QApplication
 
 
@@ -6,6 +7,42 @@ app = QApplication([])
 
 
 from main_window import *
+
+class Question():
+    def __init__(self, question_text, answer_text, wrong:tuple) -> None:
+        self.question_text = question_text
+        self.answer_text = answer_text
+        self.wrong_answer = wrong
+        
+    def got_right(self):
+        ...
+    def got_wrong(self):
+        ...
+
+q1 = Question("Ярік", "jarik", ("appolon 11", "cigan", "lochara"))
+q2 = Question("Сердючка", "man",("women", "fig zna", "dima"))
+q3 = Question("коли діма піде на тренування", "when  will he wont to go",("never", "after 15 years" ,"I dont know"))
+q4 = Question("коли я получу 12", "коли нормалбно вивчу тему",("коли будуть нормальні вчителі", "коли діма підe на тренування", "коли перестану тупити і втикати в дошку"))
+
+question_list = [q1, q2, q3, q4]
+radio_button_list = [rb_answer_1, rb_answer_2, rb_answer_3, rb_answer_4]
+
+
+def new_question():
+    random_question =choice(question_list)
+
+
+    shuffle(radio_button_list)
+
+    question_lb.setText(random_question.question_text)
+
+    radio_button_list[0].setText(random_question.answer_text)
+    for i in  range(3):
+      radio_button_list[i + 1].setText(random_question.wrong_answer[i])
+
+ 
+
+new_question()
 
 
 def chandge_box():
@@ -17,6 +54,8 @@ def chandge_box():
         radio_button_box.show()
         answer_box.hide()
         button_next.setText("Відповісти")
+
+        new_question()
 
 
 button_next.clicked.connect(chandge_box)
